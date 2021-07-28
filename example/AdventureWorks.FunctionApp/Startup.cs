e131dc95c1a2c668d157b1b4c8efcd5c796615f7
patrickhuber <patrickhuber@users.noreply.github.com>
2021-07-28 14:25:35 -0400

    adds function startup

---
+++ example/AdventureWorks.FunctionApp/Startup.cs
@@ -0,0 +1,21 @@
+﻿using AdventureWorks.Cosmos;
+using AdventureWorks.Logical.PersonDelete;
+using AdventureWorks.Logical.PersonRead;
+using AdventureWorks.Logical.PersonWrite;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(AdventureWorks.FunctionApp.Startup))]
+
+namespace AdventureWorks.FunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {            
+            builder.Services.AddSingleton<IPersonRead, CosmosPersonService>();
+            builder.Services.AddSingleton<IPersonWrite, CosmosPersonService>();
+            builder.Services.AddSingleton<IPersonDelete, CosmosPersonService>();
+        }
+    }
+}
