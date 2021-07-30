ace3145f259b65e329d12844edcbad40fb80a833
patrickhuber <patrickhuber@users.noreply.github.com>
2021-07-30 14:12:37 -0400

    adds CosmosPersonService implementation registration

--- example/AdventureWorks.FunctionApp/Startup.cs
+++ example/AdventureWorks.FunctionApp/Startup.cs
@@ -2,6 +2,7 @@
 using AdventureWorks.Logical.PersonDelete;
 using AdventureWorks.Logical.PersonRead;
 using AdventureWorks.Logical.PersonWrite;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,7 +13,13 @@ namespace AdventureWorks.FunctionApp
     public class Startup : FunctionsStartup
     {
         public override void Configure(IFunctionsHostBuilder builder)
-        {            
+        {
+            builder.Services.AddSingleton(services => 
+            {
+                var cosmosClient = new CosmosClient("Cosmos:ConnectionString");
+                return new CosmosPersonService(cosmosClient);
+            });
+
             builder.Services.AddSingleton<IPersonRead, CosmosPersonService>();
             builder.Services.AddSingleton<IPersonWrite, CosmosPersonService>();
             builder.Services.AddSingleton<IPersonDelete, CosmosPersonService>();
